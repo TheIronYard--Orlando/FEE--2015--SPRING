428b131bc52fff6fcc98da529bab8d303c077c65
David Rogers <al.the.x@gmail.com>
2015-01-27 13:36:22 -0500

    More gymnastics with `forEach`...

--- Notes/2015-01-27/scripts/gymnastics.js
+++ Notes/2015-01-27/scripts/gymnastics.js
@@ -3,17 +3,64 @@ var assert = require('assert');
 var events = require('../apis/github/users/al-the-x/events.json');
 
 function answer(){
-  return {
-    'total': events.length
+  var index = 0;
+  var numberOfPushEvents = 0;
+
+  events.forEach(function(item){
+    if ( item.type == "PushEvent" ){
+      numberOfPushEvents++;
+    }
+  });
+  var pushEvents = events.filter(function(item){
+    return item.type == 'PushEvent';
+  });
+  
+  pushEvents.length == numberOfPushEvents;
+
+  /* // Irrelevant in the face of forEach...
+  while ( index < events.length ){
+    var event = events[index];
+
+    if ( event.type == "PushEvent" ){
+      numberOfPushEvents++;
+    }
+    
+    index++;
   }
+  // */
+
+  return {
+    'total': events.length,    // How many total events did you fetch?
+    'PushEvent': {
+      'total': numberOfPushEvents,
+    }
+    /* // Coming back around...
+    'PushEvent': {
+      'total': ...,  // How many total events of type `PushEvent` are there?
+      'perDay': ...  // On average, how many`PushEvent` entries per day?
+    },
+    'other': {
+      'total': ...,  // How many _other_ events are in the data?
+      'perDay': ...  // How many per day, on average?
+    } // */
+  };
 } // END answer
 
-assert(events);
-assert(answer);
-assert(typeof answer == 'function');
+it('should have one big test', function(){
+  assert(events);
+  assert(events instanceof Array);
 
-var theAnswer = answer();
-  
-assert.equal(theAnswer.total, 30);
-assert(theAnswer.PushEvents);
-assert(theAnswer.PushEvents.total);
+  assert(answer);
+  assert(typeof answer == 'function');
+
+  var theAnswer = answer();
+
+  assert.equal(theAnswer.total, 30);
+  // I assert that the key "total" in variable 
+  // "theAnswer" is equal to Number 30
+
+  assert(theAnswer.PushEvent);
+
+  assert(theAnswer.PushEvent.total > 0);
+  // */
+});
