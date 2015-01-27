4e8eab6ef24aa501fab963a7aa91af07e068d768
David Rogers <al.the.x@gmail.com>
2015-01-27 10:51:38 -0500

    Starting point...

---
+++ Notes/2015-01-27/scripts/string-calculator.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+
+console.log(Date());
+
+it('should fail', function(){
+  assert(true);
+});
+
+/**
+ * Create a function called `plus()` that accepts two `Strings`
+ * representing English number words, e.g. "one", "two", "three", 
+ * and returns a `Number` representing their sum. For example:
+ *
+ *     assert.equal(plus("one", "one"), 2);
+ *
+ * @param String A English number word
+ * @param String B English number word
+ * @return Number sum of A and B
+ */
+function plus(A, B){
+} // END plus
