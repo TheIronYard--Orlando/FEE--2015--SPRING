4241b445a1656b98d968b8647ab7f7f571b29999
David Rogers <al.the.x@gmail.com>
2015-02-11 12:34:22 -0500

    Hello from Vue!

--- Notes/2015-02-11/js/main.js
+++ Notes/2015-02-11/js/main.js
@@ -1,1 +1,10 @@
+var vm = new Vue({
+  el: document.getElementById('chessboard'),
+  data: {
+    message: 'Hello from Vue'
+  }
+});
 
+setTimeout(function(){
+  vm.message = 'Do you like my hat?';
+}, 3000);
