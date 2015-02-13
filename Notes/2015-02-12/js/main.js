ae7fd50728418bf4ec7ed5afa33aab5ee97c9917
David Rogers <al.the.x@gmail.com>
2015-02-13 14:45:17 -0500

    Step 9c: Repeating more intelligently...

--- Notes/2015-02-12/js/main.js
+++ Notes/2015-02-12/js/main.js
@@ -2,22 +2,36 @@
   new Vue({
     el: '#vue-panel',
     data: {
-      greens: 4,
-      yellows: 2,
-      reds: 1,
-      total: 11
+      assignments: [
+        { title: "Assignment 00: Brace Yourself",
+          greens: 4,
+          yellows: 2,
+          reds: 1,
+          total: 11
+        },
+        { title: "Assignment 01: MF'g Website",
+          greens: 11,
+          yellows: 0,
+          reds: 0,
+          total: 11
+        },
+        { title: "Assignment 02: Learning Layout",
+          greens: 0,
+          yellows: 8,
+          reds: 3,
+          total: 11
+        }
+      ]
     },
     methods: {
-      percent: function(part){
-        return Math.floor(part / this.total * 100);
-      }
-    },
-    computed: {
-      percentTotal: function(){
+      percent: function(part, total){
+        return Math.floor(part / total * 100);
+      },
+      percentTotal: function(assignment){
         return (
-          this.percent(this.greens) +
-          this.percent(this.yellows) +
-          this.percent(this.reds)
+          this.percent(assignment.greens, assignment.total) +
+          this.percent(assignment.yellows, assignment.total) +
+          this.percent(assignment.reds, assignment.total)
         );
       }
     }
