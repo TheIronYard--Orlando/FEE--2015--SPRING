37c566fbb5897f8b9914b8d2f7cb2717473428ad
David Rogers <al.the.x@gmail.com>
2015-02-13 15:11:19 -0500

    Step 10b: Refactoring to an external JSON file...

--- Notes/2015-02-12/js/main.js
+++ Notes/2015-02-12/js/main.js
@@ -30,29 +30,9 @@
     }
   });
 
-  setTimeout(function(){
-    vm.assignments = [
-      { title: "Assignment 00: Brace Yourself",
-        greens: 4,
-        yellows: 2,
-        reds: 1,
-        total: 11
-      },
-      { title: "Assignment 01: MF'g Website",
-        greens: 11,
-        yellows: 0,
-        reds: 0,
-        total: 11
-      },
-      { title: "Assignment 02: Learning Layout",
-        greens: 0,
-        yellows: 8,
-        reds: 3,
-        total: 11
-      }
-    ];
-
-  }, 3000)
+  jQuery.getJSON('apis/assignments.json').done(function(assignments){
+    vm.assignments = assignments;
+  });
 
   var $panel = jQuery('#jquery-panel');
 
