4c91cc6c5a170448ebaf52435c345f92f3e1688c
David Rogers <al.the.x@gmail.com>
2015-02-13 14:23:55 -0500

    Step 9b: Doing some calculations with Vue...

--- Notes/2015-02-12/js/main.js
+++ Notes/2015-02-12/js/main.js
@@ -1,5 +1,29 @@
 (function(){
   new Vue({
-    el: '.panel.panel-default'
+    el: '#vue-panel',
+    data: {
+      greens: 4,
+      yellows: 2,
+      reds: 1,
+      total: 11
+    },
+    methods: {
+      percent: function(part){
+        return Math.floor(part / this.total * 100);
+      }
+    },
+    computed: {
+      percentTotal: function(){
+        return (
+          this.percent(this.greens) +
+          this.percent(this.yellows) +
+          this.percent(this.reds)
+        );
+      }
+    }
   });
+
+  var $panel = jQuery('#jquery-panel');
+
+  $panel.after($panel.clone());
 })();
