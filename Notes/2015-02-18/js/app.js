f82dd56624f31a852db0c9aec29087019e13e653
David Rogers <al.the.x@gmail.com>
2015-02-18 13:19:03 -0500

    Adding `localStorage` example...

--- Notes/2015-02-18/js/app.js
+++ Notes/2015-02-18/js/app.js
@@ -1,30 +1,49 @@
 (function (window) {
 	'use strict';
 
-	var listOfTasks = [
-		{ text: 'Taste JavaScript',
-			done: true, editing: false },
-		{ text: 'Buy a unicorn',
-		  done: false, editing: false },
-	];
-
+  if ( !getTasks().length ){
+		// Create some dummy tasks if none exist...
+		// FIXME: remove when "create task" works...
+		setTasks([
+			{ text: 'Taste JavaScript',
+				done: true, editing: false },
+			{ text: 'Buy a unicorn',
+			  done: false, editing: false },
+		]);
+	}
 
 	new Vue({
 		el: '#todoapp',
 		data: {
-			"tasks": listOfTasks
+			tasks: getTasks()
 		},
 		methods: {
 			remove: function(task){
 				this.tasks.splice(this.tasks.indexOf(task), 1);
+				setTasks(this.tasks);
 			},
 			edit: function(task){
 				task.editing = true;
 			},
-			unedit: function(task){
+			save: function(task){
 				task.editing = false;
+				setTasks(this.tasks);
 			}
 		}
-	});
+	}); // END new Vue
+
+	/**
+	 * @returns Array
+	 */
+	function getTasks(){
+		return JSON.parse(localStorage.getItem('tasks')) || [ ];
+	}
+
+	/**
+	 * @param Array of Object representing tasks
+	 */
+	function setTasks(tasks){
+		localStorage.setItem('tasks', JSON.stringify(tasks));
+	}
 
 })(window);
