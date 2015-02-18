e047056813889a5a326b0c4106a61b2cca4ac653
David Rogers <al.the.x@gmail.com>
2015-02-18 13:09:05 -0500

    Stateless tasks with Vue

--- Notes/2015-02-18/js/app.js
+++ Notes/2015-02-18/js/app.js
@@ -1,6 +1,30 @@
 (function (window) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
+	var listOfTasks = [
+		{ text: 'Taste JavaScript',
+			done: true, editing: false },
+		{ text: 'Buy a unicorn',
+		  done: false, editing: false },
+	];
+
+
+	new Vue({
+		el: '#todoapp',
+		data: {
+			"tasks": listOfTasks
+		},
+		methods: {
+			remove: function(task){
+				this.tasks.splice(this.tasks.indexOf(task), 1);
+			},
+			edit: function(task){
+				task.editing = true;
+			},
+			unedit: function(task){
+				task.editing = false;
+			}
+		}
+	});
 
 })(window);
