330090c57ccc161d054a24f3a1c81ac2d649f050
David Rogers <al.the.x@gmail.com>
2015-03-04 12:28:04 -0500

    Results of `yo angular`, unaltered...

---
+++ Notes/2015-03-04/app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name 20150304App
+ * @description
+ * # 20150304App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('20150304App', []);
---
+++ Notes/2015-03-04/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 20150304App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the 20150304App
+ */
+angular.module('20150304App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
